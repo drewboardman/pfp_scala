6bb1ca700ffa47acf3ce2c2cd789bf0f04cd3325
Drew Boardman <drew.e.boardman@gmail.com>
2020-12-11 18:39:45 -0600

    more

---
+++ modules/core/src/main/scala/shop/effects/CommonEffects.scala
@@ -0,0 +1,11 @@
+package shop.effects
+
+import cats.MonadError
+
+object CommonEffects {
+  type MonadThrow[F[_]] = MonadError[F, Throwable]
+
+  object MonadThrow {
+    def apply[F[_]](implicit ev: MonadError[F, Throwable]): MonadThrow[F] = ev
+  }
+}
